6c38a6985e2b1baef21e48a8e055de1c43586509
Newbit13 <525842854@qq.com>
2021-07-23 09:51:36 +0800

    no message

--- pages/main/main.js
+++ pages/main/main.js
@@ -1,73 +1,79 @@
 // pages/main.js
-const app = getApp()
+const app = getApp();
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    fps: 30,
-    resultFps: 0,
+    fps: 50,
+    // resultFps: 0,
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    this.flagTimer && clearInterval(this.flagTimer)
+    this.flagTimer && clearInterval(this.flagTimer);
   },
   // 点操作
-  cameraFrame() { // 视频流
+  cameraFrame() {
+    // 视频流
     var that = this,
       store = [],
-      startTime = new Date(),
-      camera = wx.createCameraContext()
+      // startTime = new Date(),
+      camera = wx.createCameraContext();
     that.listener = camera.onCameraFrame(function (frame) {
       // console.log(frame);
-      if (frame && app.globalData.movenet) { //帧率控制
-        store.push(frame)
+      if (frame && app.globalData.movenet) {
+        //帧率控制
+        store.push(frame);
       }
-    })
+    });
     that.listener.start({
       success: function () {
-        that.flagTimer && clearInterval(that.flagTimer)
-        // console.log("fps");
-        // console.log(1000 / that.data.fps);
-        that.flagTimer = setInterval(function () { //帧率控制
+        that.flagTimer && clearInterval(that.flagTimer);
+        that.flagTimer = setInterval(function () {
+          //帧率控制
           if (store.length == 0) return;
           var object = {
             data: new Uint8Array(store[store.length - 1].data),
             height: Number(store[store.length - 1].height),
-            width: Number(store[store.length - 1].width)
-          }
-          that.actionSend(object)
-          store = []
-          that.setData({
-            resultFps: that.data.resultFps + 1,
-            fpstime: parseInt((that.data.resultFps + 1) * 1000 / (new Date().getTime() - startTime))
-          })
-        }, 1000 / that.data.fps)
+            width: Number(store[store.length - 1].width),
+          };
+          that.actionSend(object);
+          store = [];
+          // that.setData({
+          //   resultFps: that.data.resultFps + 1,
+          //   fpstime: parseInt((that.data.resultFps + 1) * 1000 / (new Date().getTime() - startTime))
+          // })
+        }, 1000 / that.data.fps);
       },
-    })
-  },
-
-  actionSend(object) { // 识别点
-    const that = this
-    // console.log("object开始");
-    // console.log(object);
-    app.globalData.movenet.estimatePoses(object).then(function (res) {
-      // console.log(res);
-      var ctx = that.ctx,
-        keypoimts = res[0].keypoints
-      // console.log(keypoimts);
-      ctx.clearRect(0, 0, that.canvas.width, that.canvas.height)
-      that.drawSkevaron(keypoimts)
-      that.drawKeypoints(keypoimts)
-    }).catch(function (err) {
-      console.log(err)
     });
   },
 
-  drawSkevaron(keypoints, scale = 1) { // 关键点连线
+  actionSend(object) {
+    // 识别点
+    const that = this;
+    // console.log("object开始");
+    // console.log(object);
+    app.globalData.movenet
+      .estimatePoses(object)
+      .then(function (res) {
+        // console.log(res);
+        var ctx = that.ctx,
+          keypoimts = res[0].keypoints;
+        // console.log(keypoimts);
+        ctx.clearRect(0, 0, that.canvas.width, that.canvas.height);
+        that.drawSkevaron(keypoimts);
+        that.drawKeypoints(keypoimts);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  },
+
+  drawSkevaron(keypoints, scale = 1) {
+    // 关键点连线
     // 头部
     this.drawSegment(keypoints[0], keypoints[1]);
     this.drawSegment(keypoints[0], keypoints[2]);
@@ -89,7 +95,8 @@ Page({
     this.drawSegment(keypoints[13], keypoints[15]);
   },
 
-  drawSegment(akeypoints, bkeypoints) { // 画线
+  drawSegment(akeypoints, bkeypoints) {
+    // 画线
     // var ax = akeypoints[0],
     //   ay = akeypoints[1],
     //   bx = bkeypoints[0],
@@ -97,17 +104,18 @@ Page({
     var ax = akeypoints.x,
       ay = akeypoints.y,
       bx = bkeypoints.x,
-      by = bkeypoints.y
+      by = bkeypoints.y;
     this.ctx.beginPath();
     this.ctx.moveTo(ax, ay);
     this.ctx.lineTo(bx, by);
     this.ctx.lineWidth = 3;
-    this.ctx.strokeStyle = '#ff0000';
+    this.ctx.strokeStyle = "#ff0000";
     this.ctx.stroke();
     this.ctx.restore();
   },
 
-  drawKeypoints(keypoints) { // 画关键点
+  drawKeypoints(keypoints) {
+    // 画关键点
     // console.log(keypoints);
     for (var i = 0; i < keypoints.length; i++) {
       var keypoint = keypoints[i];
@@ -116,43 +124,46 @@ Page({
     }
   },
 
-  drawPoint(y, x) { // canvas画点
+  drawPoint(y, x) {
+    // canvas画点
     this.ctx.beginPath();
     this.ctx.arc(x, y, 4, 0, 2 * Math.PI, false);
-    this.ctx.lineWidth = 2
-    this.ctx.strokeStyle = '#ff00ff'
-    this.ctx.fillStyle = '#00ff66'
+    this.ctx.lineWidth = 2;
+    this.ctx.strokeStyle = "#ff00ff";
+    this.ctx.fillStyle = "#00ff66";
     this.ctx.fill();
-    this.ctx.stroke()
+    this.ctx.stroke();
     this.ctx.restore();
   },
 
-  canvasInit(cb) { // 获取canvas
-    var that = this
-    wx.createSelectorQuery().select('#myCanvas')
+  canvasInit(cb) {
+    // 获取canvas
+    var that = this;
+    wx.createSelectorQuery()
+      .select("#myCanvas")
       .fields({
         node: true,
-        size: true
+        size: true,
       })
       .exec(function (res) {
-        var canvas = res[0].node
-        var ctx = canvas.getContext('2d')
+        var canvas = res[0].node;
+        var ctx = canvas.getContext("2d");
 
-        var dpr = wx.getSystemInfoSync().pixelRatio
-        canvas.width = 375 * dpr
-        canvas.height = 640 * dpr
-        ctx.scale(dpr, dpr)
+        // var dpr = wx.getSystemInfoSync().pixelRatio
+        // canvas.width = 375 * dpr
+        // canvas.height = 640 * dpr
+        // ctx.scale(dpr, dpr)
 
-        // wx.getSystemInfo({
-        //   success: function (res) {
-        //     console.log(res);
-        //     canvas.width = res.windowWidth
-        //     canvas.height = res.windowHeight
-        //   },
-        // })
-        that.ctx = ctx
-        that.canvas = canvas
-        that.res0 = res[0]
+        wx.getSystemInfo({
+          success: function (res) {
+            // console.log(res);
+            canvas.width = res.windowWidth;
+            canvas.height = res.windowHeight;
+          },
+        });
+        that.ctx = ctx;
+        that.canvas = canvas;
+        that.res0 = res[0];
 
         // that.drawPoint(320, 320)
         // that.drawKeypoints([
@@ -161,8 +172,8 @@ Page({
         // ])
         // that.drawSegment([0,0],[100,200])
 
-        cb();//注册视频流
-      })
+        cb(); //注册视频流
+      });
   },
   /**
    * 生命周期函数--监听页面加载
@@ -170,8 +181,8 @@ Page({
   onLoad: function (options) {
     const that = this;
     this.canvasInit(function () {
-      that.cameraFrame()
-    })
+      that.cameraFrame();
+    });
     // this.cameraFrame()
   },
-})
+});
